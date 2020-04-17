f03395be07154408b450741ca3037b8b4a4c6b0e
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-17 21:03:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog 
+  
+  attr_accessor :name, :breed 
+  attr_reader :id
+  
+  def initialize(id: nil, name:, breed:)
+    @name = name
+    @breed = breed
+  end  
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+  end  
+  
+  def self.drop_table
+   DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
+  end  
+  
+  def save 
+   
+  end  
+  
+  
+end
