fdae1f3fdc5c5f5f40cccc5e72a772f510136d4b
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-18 00:11:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog 
   
   attr_accessor :name, :breed 
@@ -23,9 +25,41 @@ class Dog
    DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
   end  
   
-  def save 
-   
+  def save
+   if self.id == true
+       update
+    else
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+   end
+    self #returns an instance of the dog class
+  end 
+  
+  def self.create(name:, breed:) 
+   dog = Dog.new(name: name, breed: breed) 
+   dog.save
+   dog
   end  
+  
+ #this method convert what the database gives us into a object.we're reading data from SQLite and temporarily representing that data in Ruby
+  def self.new_from_db(row)
+    hash = {
+      name: row[1],
+      breed: row[2]
+    }
+    self.create(hash)
+  end
+  
+  def self.find_by_id
+  
+  end  
+  
+  
   
   
 end
