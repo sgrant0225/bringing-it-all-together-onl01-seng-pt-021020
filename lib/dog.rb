da8bf0b8d2d8706ff66d16fa6dd94ea3a9c04428
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-18 13:07:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,11 +56,10 @@ class Dog
   end
   
   def self.find_by_id(id_x) #pass in the id_x parameter
-    dog = DB[:conn].execute("select * from dogs where id = ?", id_x).first #
+    dog = DB[:conn].execute("select * from dogs where id = ?", id_x).first #pass it in here #.first pulls out the 1st element
     dog = Dog.new(id: dog[0], name: dog[1], breed: dog[2])
-     binding.pry
     dog
-   
+   binding.pry
   end  
   
   
